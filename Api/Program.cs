9d1e61cdbf3d51a63a5cf5554842701de95fc891
Miguel Hipolito <miguel.hipolito.7e6@itb.cat>
2023-05-04 10:07:33 +0200

    ajhfga

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,31 +1,35 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Configuration;
+using TodoApi.Data;
+using TodoApi.Data.Repositories;
 using TodoApi.Models;
-
-DefaultTypeMap.MatchNamesWithUnderscores = true;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-builder.Services.AddDbContext<TodoContext>(opt =>
-    opt.UseInMemoryDatabase("TodoList"));
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddDbContext<PosgreSQLConfig>(opt =>
+//    opt.UseSqlServer(builder.Configuration.GetConnectionString("WebApiDatabase")));
 builder.Services.AddEndpointsApiExplorer();
+
+
+builder.Services.AddSingleton(builder.Services.AddDbContext<PosgreSQLConfig>(options =>
+    options.UseSqlServer("WebApiDatabase")));
+
+builder.Services.AddScoped<localizacionRepository>();
+builder.Services.AddControllers();
+
+builder.Services.AddMemoryCache();
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-/////////////////////hgdfdfddfgfd
-app.UseDefaultFiles();
-app.UseStaticFiles();
 
 app.UseHttpsRedirection();
 
